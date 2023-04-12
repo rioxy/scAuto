d4de526a3cdb2ac1215d184c4b649d0a51705714
rioxy <88192614+rioxy@users.noreply.github.com>
2023-04-12 19:49:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
 echo "   - Autoreboot On 00.00 GMT +7" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "------------------------------------------Lost Server------------------------------------------" | tee -a log-install.txt
+echo "------------------------------------------rioxy------------------------------------------" | tee -a log-install.txt
 echo ""
 echo " Reboot 10 Sec"
 sleep 10

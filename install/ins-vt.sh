7b554f32455669ce0726094d0d2c7e2269419f31
rioxy <88192614+rioxy@users.noreply.github.com>
2023-04-12 18:41:56 +0700

    Update ins-vt.sh

--- install/ins-vt.sh
+++ install/ins-vt.sh
@@ -513,22 +513,22 @@ systemctl start v2ray@trojan.service
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O addws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addws.sh"
-wget -O addvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addvless.sh"
-wget -O addtr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addtr.sh"
-wget -O delws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/delws.sh"
-wget -O delvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/delvless.sh"
-wget -O deltr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/deltr.sh"
-wget -O cekws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cekws.sh"
-wget -O cekvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cekvless.sh"
-wget -O cektr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cektr.sh"
-wget -O renewws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewws.sh"
-wget -O renewvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewvless.sh"
-wget -O renewtr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewtr.sh"
-wget -O xp-ws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-ws.sh"
-wget -O xp-tr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-tr.sh"
-wget -O xp-vless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-vless.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cert.sh"
+wget -O addws "https://raw.githubusercontent.com/rioxy/scAuto/main/task/addws.sh"
+wget -O addvless "https://raw.githubusercontent.com/rioxy/scAuto/main/task/addvless.sh"
+wget -O addtr "https://raw.githubusercontent.com/rioxy/scAuto/main/task/addtr.sh"
+wget -O delws "https://raw.githubusercontent.com/rioxy/scAuto/main/task/delws.sh"
+wget -O delvless "https://raw.githubusercontent.com/rioxy/scAuto/main/task/delvless.sh"
+wget -O deltr "https://raw.githubusercontent.com/rioxy/scAuto/main/task/deltr.sh"
+wget -O cekws "https://raw.githubusercontent.com/rioxy/scAuto/main/task/cekws.sh"
+wget -O cekvless "https://raw.githubusercontent.com/rioxy/scAuto/main/task/cekvless.sh"
+wget -O cektr "https://raw.githubusercontent.com/rioxy/scAuto/main/task/cektr.sh"
+wget -O renewws "https://raw.githubusercontent.com/rioxy/scAuto/main/task/renewws.sh"
+wget -O renewvless "https://raw.githubusercontent.com/rioxy/scAuto/main/task/renewvless.sh"
+wget -O renewtr "https://raw.githubusercontent.com/rioxy/scAuto/main/task/renewtr.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/rioxy/scAuto/main/task/xp-ws.sh"
+wget -O xp-tr "https://raw.githubusercontent.com/rioxy/scAuto/main/task/xp-tr.sh"
+wget -O xp-vless "https://raw.githubusercontent.com/rioxy/scAuto/main/task/xp-vless.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/rioxy/scAuto/main/task/cert.sh"
 chmod +x addws
 chmod +x addvless
 chmod +x addtr
